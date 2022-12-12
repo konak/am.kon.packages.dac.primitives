e911b3a67d70dcd5af778594a120e858220683bd
sergey konakov <konakov.sergey@gmail.com>
2022-12-12 21:07:43 +0400

    1. Add package description properties to project file to generate nuget package 2. Rename property name

--- Config/DacConfig.cs
+++ Config/DacConfig.cs
@@ -6,7 +6,7 @@ namespace am.kon.packages.dac.primitives.Config
 		/// <summary>
 		/// Data Access Component configuration default name
 		/// </summary>
-		public const string SectionNane = "am.kon.dac";
+		public const string SectionDefaultName = "am.kon.dac";
 
 		/// <summary>
 		/// Property identifying the default connection string registered in configuration file
